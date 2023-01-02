ddccb8b7e617767fe62e43abbe2652aa80e3491f
JoshFrederickLocal <50629845+JoshuaFrederick@users.noreply.github.com>
2023-01-02 07:35:13 -0500

    Fixed set_mode method

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,7 +7,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set.mode((1200,800))
+        self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
